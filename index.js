a278e075072a1b255d46a378a5cec148336f4571
Prashanth Mittakola <66918475+prashanth-mittakola@users.noreply.github.com>
2021-04-22 11:55:39 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const arrows = document.querySelectorAll("i.arrow");
+const movieList = document.querySelectorAll(".movie-list");
+
+arrows.forEach((arrow, i) => {
+  const movieItemsLength = movieList[i].querySelectorAll("img").length;
+  let clickCounter = 0;
+  arrow.addEventListener("click", () => {
+    clickCounter++;
+    let valueOfX = movieList[i].computedStyleMap().get("transform")[0].x.value;
+    if (movieItemsLength - (5 + clickCounter) >= 0) {
+      movieList[i].style.transform = `translateX(${valueOfX - 290}px)`;
+    } else {
+      movieList[i].style.transform = "translateX(0)";
+      clickCounter = 0;
+    }
+  });
+});
+
+// For White Theme
+const toggleBall = document.querySelector(".toggle");
+const toToggleItems = document.querySelectorAll(
+  ".toggle, .toggle-ball, h2, .sidebar, .navbar-container, .container"
+);
+toggleBall.addEventListener("click", () => {
+  toToggleItems.forEach((item) => {
+    item.classList.toggle("active");
+  });
+});
